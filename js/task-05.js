a9591a409acc4740c57afe42af2b866c56895318
Maryna Danylchuk <marina123456mari@gmail.com>
2021-07-03 11:39:18 +0300

    правки таск 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,12 @@ let inputText = document.querySelector('#name-input');
 let nameOutput = document.querySelector('#name-output');
 
 
-
-const onInput = () => {
-  nameOutput.textContent = inputText.value ? inputText.value : 'незнакомец';
-};
-
 inputText.addEventListener('input', onInput);
+
+ function onInput () {
+    if (inputText.value === '') {
+       nameOutput.innerHTML = 'незнакомец';
+    } else{
+    nameOutput.innerHTML = inputText.value;
+    }
+}
