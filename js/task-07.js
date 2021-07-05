3865d895ed0329609e0587c6a93beadb1b5bb242
Maryna Danylchuk <marina123456mari@gmail.com>
2021-07-05 12:39:11 +0300

    правки

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,24 @@
 const getInputEl = document.querySelector('#font-size-control');
 const getSpanEl = document.querySelector('#text');
 
+getInputEl.value = 0;
+getSpanEl.style.fontSize = getInputEl.value;
+
 getInputEl.addEventListener('input', changeSizeText);
 
 function changeSizeText() {
        
        getSpanEl.style.fontSize = getInputEl.value + "px";
 }
+ /*=============2 вариант, усовершенствованный===========*/
+
+// let getInputEl = document.querySelector('#font-size-control');
+// let getSpanEl = document.querySelector('#text');
+
+// getInputEl.min = getInputEl.value = "10";
+// getInputEl.max = "30";
+
+// getSpanEl.style.fontSize = `${getInputEl.min}px`;
+
+
+// getInputEl.addEventListener('input', () => { getSpanEl.style.fontSize = `${getInputEl.value}px`});
